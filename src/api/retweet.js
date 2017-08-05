eea6208c6a4af80ccbaa2b8cf37b46697db98465
spences10 <spences10apps@gmail.com>
2017-08-05 19:42:59 +0100

    use bot instead of T

--- src/api/retweet.js
+++ src/api/retweet.js
@@ -4,19 +4,21 @@ const config = require('../config')
 const paramters = require('./parameters')
 const twit = require('twit')
 
-const T = new twit(config.twitter)
+const bot = new twit(config.twitter)
 
 const retweet = () => {
   let params = paramters
 
-  T.get('search/tweets', params, (err, data)=> {
+  bot.get('search/tweets', params, (err, data) => {
     // grab tweet ID to retweet
     let retweetId = data.statuses[0].id_str
 
-    if(err) console.log('ERROR: Cannot Search Tweet!')
+    if (err) console.log('ERROR: Cannot Search Tweet!')
 
-    T.post('statuses/retweet/:id', {id: retweetId}, (err, response) => {
-      if(err) {console.log('ERROR: Retweet!')}
+    bot.post('statuses/retweet/:id', { id: retweetId }, (err, response) => {
+      if (err) {
+        console.log('ERROR: Retweet!')
+      }
       console.log('SUCCESS: Retweet')
     })
   })
