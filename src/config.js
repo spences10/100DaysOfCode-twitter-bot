1c658c674d00a1244a34dfa65c1393c7d9f8b5bc
spences10 <spences10apps@gmail.com>
2017-08-05 07:33:28 +0100

    update config

--- src/config.js
+++ src/config.js
@@ -8,15 +8,26 @@
 require('dotenv').config()
 
 module.exports = {
-  twitter: {
+  twitterKeys: {
     consumer_key: process.env.TWITTER_CONSUMER_KEY,
     consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
     access_token: process.env.TWITTER_ACCESS_TOKEN,
-    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
+    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+  },
+  twitterConfig: {
+    queryString: process.env.QUERY_STRING,
+    resultType: process.env.RESULT_TYPE,
+    language: process.env.LANGUAGE,
     username: process.env.TWITTER_USERNAME,
     retweet: process.env.TWITTER_RETWEET_RATE * 1000 * 60,
-    favorite: process.env.TWITTER_FAVORITE_RATE * 1000 * 60
+    follow: process.env.TWITTER_FOLLOW_RATE * 1000 * 60,
+    searchCount: process.env.TWITTER_SEARCH_COUNT,
+    randomReply: process.env.RANDOM_REPLY,
+    randomEmoji: process.env.RANDOM_EMOJI,
+    blacklist: process.env.TWITTER_USERNAME_BLACKLIST
   },
-  sentiment_api_key: process.env.SENTIMENT_KEY,
-  queryString: process.env.QUERY_STRING
+  graphqlConfig: {
+    endpoint: process.env.ENDPOINT,
+    project: process.env.ACCESS_TOKEN
+  }
 }
