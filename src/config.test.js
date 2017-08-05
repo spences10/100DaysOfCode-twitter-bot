8d714fab7d997e7bbff8b2193182b1e3fce7c33c
spences10 <spences10apps@gmail.com>
2017-08-05 19:42:43 +0100

    update test

--- src/config.test.js
+++ src/config.test.js
@@ -1,8 +1,8 @@
-const config = require('./config').twitter
+const config = require('./config').twitterKeys
 
 test('returns config object', () => {
   expect(config.access_token).not.toBe(''),
-  expect(config.access_token_secret).not.toBe(''),
-  expect(config.consumer_key).not.toBe(''),
-  expect(config.consumer_secret).not.toBe('')
+    expect(config.access_token_secret).not.toBe(''),
+    expect(config.consumer_key).not.toBe(''),
+    expect(config.consumer_secret).not.toBe('')
 })
