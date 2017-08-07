e927fa1c1ab454fd1aef9c3c57e02fe4824b1d82
spences10 <spences10apps@gmail.com>
2017-08-07 07:20:27 +0100

    LOGIC!

--- src/helpers/dbHandleSentiment.js
+++ src/helpers/dbHandleSentiment.js
@@ -1,5 +1,6 @@
 const dbAddSentiment = require('./dbAddSentiment')
 const checkTweet = require('./dbCheckTweet')
+const sentiment = require('../api/sentiment')
 
 const config = require('../config')
 
@@ -11,15 +12,28 @@ const handleSentiment = (event) => {
   console.log('EVENT USER NAME: ', event.user.screen_name)
   console.log('INDEX OF NAME IN BLACKLIST: ', blacklist.indexOf(event.user.screen_name))
   console.log('====================')
+  console.log('====================')
+  console.log('First check: ', event.lang.toString().trim() === config.twitterConfig.language.toString().trim())
+  console.log('First check: event lang=', event.lang.toString().trim())
+  console.log('First check: config lang=', config.twitterConfig.language.toString().trim())
+  console.log('Second check: ', !event.in_reply_to_status_id)
+  console.log('Third check: ', blacklist.indexOf(event.screen_name) < 0)
+  console.log('====================')
   if (
-    event.lang != config.twitterConfig.language ||
-    !event.in_reply_to_status_id ||
-    blacklist.indexOf(event.screen_name) > -1
+    event.lang.toString().trim() === config.twitterConfig.language.toString().trim() ||
+    event.in_reply_to_status_id ||
+    blacklist.indexOf(event.screen_name) < 0
   ) {
-    return
+    // do the thing
   } else {
+    console.log('====================')
+    console.log('HERE return')
+    console.log('====================')
+    return
     // console.log(JSON.stringify(event.lang))
     // console.log(JSON.stringify(event))
+    sentiment()
+    // dbAddSentiment()
     // checkTweet(event).then((data) => {
     //   let count = data.length
     //   if (!count >= 0) {
